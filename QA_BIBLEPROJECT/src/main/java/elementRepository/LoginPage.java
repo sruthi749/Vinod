95044fc5f3a990536116552137032d27c11d212a
HP <HP@DESKTOP-O53ONDR>
2025-01-22 19:15:37 +0530

    commit 22.01.2025

--- QA_BIBLEPROJECT/src/main/java/elementRepository/LoginPage.java
+++ QA_BIBLEPROJECT/src/main/java/elementRepository/LoginPage.java
@@ -7,12 +7,15 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 import utilities.GeneralUtilities;
+import utilities.WaitUtility;
 
 public class LoginPage {
 
 	WebDriver driver;
 	GeneralUtilities utilities=new GeneralUtilities();
-
+	
+	WaitUtility wait = new WaitUtility(); 
+	
 	public LoginPage(WebDriver driver) {
 		this.driver = driver;
 		PageFactory.initElements(driver, this);
@@ -64,6 +67,7 @@ public class LoginPage {
 	{
 		inputUserName(uname);
 		inputPassword(password);
+		wait.presenceOfElementLocate(driver, "//button[@class='btn btn-default']");
 		clickLoginButton();
 		return new DashboardPage(driver);
 	}
---
+++ QA_BIBLEPROJECT/src/main/java/utilities/WaitUtility.java
@@ -0,0 +1,18 @@
+package utilities;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class WaitUtility {
+
+		public void presenceOfElementLocate(WebDriver driver,String locator) {
+			WebDriverWait explicitwait=new WebDriverWait(driver, Duration.ofMillis(10000));
+			explicitwait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.xpath(locator)));
+			
+			
+		}
+}
