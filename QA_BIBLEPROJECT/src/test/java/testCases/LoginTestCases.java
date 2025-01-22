1bbe1052eb0ac2146c199db2c5098355be044ec9
HP <HP@DESKTOP-O53ONDR>
2025-01-22 20:40:07 +0530

    commit 22.01.2025 8.40pm

--- QA_BIBLEPROJECT/src/test/java/testCases/LoginTestCases.java
+++ QA_BIBLEPROJECT/src/test/java/testCases/LoginTestCases.java
@@ -10,18 +10,18 @@ import utilities.ExcelReadUtility;
 
 public class LoginTestCases extends BaseClass {
 	// Verify login page is working
-	@Test(retryAnalyzer = retryPackage.RetryTestCases.class, groups = "Critical",dataProvider = "logindataprovider",dataProviderClass = ExcelReadUtility.class)
-	public void verifyLoginPageWorking(String username,String password) {
-		//public void verifyLoginPageWorking() {
+	@Test(retryAnalyzer = retryPackage.RetryTestCases.class, groups = "Critical")
+	// public void verifyLoginPageWorking(String username,String password) {
+	public void verifyLoginPageWorking() {
 		LoginPage lp = new LoginPage(driver);
 //		lp.inputUserName("Carol");
 //		lp.inputPassword("1q2w3e4r");
 //		lp.clickLoginButton();
-		//lp.performLogin("Carol", "1q2w3e4r");
+		// lp.performLogin("Carol", "1q2w3e4r");
 
-		//DashboardPage dp = new DashboardPage(driver);
-		DashboardPage dp = lp.performLogin(username,password);
-		//DashboardPage dp = lp.performLogin("Carol", "1q2w3e4r");
+		// DashboardPage dp = new DashboardPage(driver);
+		// DashboardPage dp = lp.performLogin(username,password);
+		DashboardPage dp = lp.performLogin("Carol", "1q2w3e4r");
 		String actualResult = dp.getTextOfWelcomeText();
 		String expectedResult = "Welcome to Payroll Application";
 
@@ -35,7 +35,7 @@ public class LoginTestCases extends BaseClass {
 		LoginPage lp = new LoginPage(driver);
 		lp.navigateToResetItLink();
 		String actualResult = lp.getTextOfSendButtonInsideResetItLink();
-		String expectedResult = "Send";
+		String expectedResult = "SEND";
 
 		Assert.assertEquals(actualResult, expectedResult,
 				"Send button inside Reset It link is not working as expected");
